ddfe6cffea519ecc6fddda043065a9ca7ebe7aef
Ny Fenitra <nyfenitra.rakoto@trente-douze.fr>
2023-01-03 09:08:06 +0300

    Adding responsive logo component

---
+++ src/components/AppLogo.vue
@@ -0,0 +1,22 @@
+<template>
+  <RouterLink to="/" class="logo d-inline-block"></RouterLink>
+</template>
+
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+</script>
+
+<style scoped lang="scss">
+.logo {
+  width: 120px;
+  height: 38px;
+  background-image: url('@/assets/svg/logo.svg');
+  background-size: contain;
+  background-position: center;
+  background-repeat: no-repeat;
+
+  &.small {
+    background-image: url('@/assets/svg/logo_adeva_symbol.svg');
+  }
+}
+</style>
