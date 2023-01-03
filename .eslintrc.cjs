111ad175d5622a7a12b278e960b4bd39d6ef20df
Ny Fenitra <nyfenitra.rakoto@trente-douze.fr>
2023-01-03 09:07:33 +0300

    Format code

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,18 @@
 /* eslint-env node */
-require("@rushstack/eslint-patch/modern-module-resolution");
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   root: true,
   extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/eslint-config-typescript",
-    "prettier",
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
+    'prettier',
   ],
   parserOptions: {
-    ecmaVersion: "latest",
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'vue/multi-word-component-names': 0,
   },
 };
